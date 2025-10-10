6919823b46a23dbcbae7f9d87e2d2f9ccc5f5cf1
Paulo <paulaotriz7@gmail.com>
2025-10-10 00:08:02 -0300

    Add Schema/Model inicial da estrutura de dados Review e criando os arquivos  app.js/Routes.js/controller.js

--- starter/model/reviewsModel.js
+++ starter/model/reviewsModel.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
-const ReviewSchema = new mongoose.Schema({});
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'O review deve ter algum conteúdo'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'O review deve ter uma avaliação'],
+    },
+    createdAt: { type: Date, default: Date.now },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'o review deve ter um tour '],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'o review deve ter um user '],
+    },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
 
-const Review = new mongoose.Model('Review', ReviewSchema);
+const Review = new mongoose.Model('Review', reviewSchema);
 module.exports = Review;
